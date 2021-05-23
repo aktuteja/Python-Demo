823b930a94f47e5cee7a6a0f5d80f76b66a150e3
aktuteja <69379016+aktuteja@users.noreply.github.com>
2021-05-23 03:23:34 -0400

    ync update

---
+++ analysis.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib as mp
+from matplotlib import pyplot as plt
+
+data = pd.read_csv("SpotifyTopPlaylist.csv")
+#print(data)
+print(data.head())
+print(data.info())
+
+artists = data['artists'].unique()
+no_of_artists = data['artists'].nunique()
+print("Artists in trending list: ", no_of_artists)
+#print(artists)
+
+#plt.plot(data.artists, data.liveness)
+#plt.show()
+
